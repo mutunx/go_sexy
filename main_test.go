aa3719247ac001fb3a2daaecb1f810dc994b8cfc
烈冰 <wsepr@163.com>
2015-01-23 13:03:09 +0800

    增加一个benchmark

--- main_test.go
+++ main_test.go
@@ -30,6 +30,8 @@ var toAbsTestData = []toAbsTest{
 	{"http://blog.hoday.cn/post/golang.html", "../tag/coding.html", "http://blog.hoday.cn/tag/coding.html"},
 }
 
+var toAbsBenchmark = []string{"?page=1", "/tag/coding.html", "tag/coding.html","http://blog.hoday.cn/post/java.html","../tag/coding.html"}
+
 func TestToAbs(t *testing.T) {
 	for _, test := range toAbsTestData {
 		p, err := url.Parse(test.pageURL)
@@ -40,4 +42,22 @@ func TestToAbs(t *testing.T) {
 			t.Errorf("toAbs(%q, %q) = %q,  期望值 %q", p, test.href, u, test.result) //调用Error不会中断
 		}
 	}
+}
+
+func BenchmarkToAbs(b *testing.B) {
+	b.StopTimer()
+	pageURL := "http://blog.hoday.cn/post/golang.html"
+	p, err := url.Parse(pageURL)
+	if err != nil {
+		b.Fatalf("url.Parse(%q) %q", pageURL, err) //调用Fatal后会中断当前的测试函数
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+        for _, val := range toAbsBenchmark{
+        	if u := toAbs(p, val); u == nil {
+				b.Fatalf("toAbs(%q, %q) 出错", p, val) //调用Error不会中断
+			}
+        }
+    }
 }
